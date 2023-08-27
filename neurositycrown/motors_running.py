209d0fbdbdd4ef6ee68f639e736cad1e301cb653
Isabel Barton <isabelbarton33@gmail.com>
2023-08-27 12:28:28 +1000

    added motors_running.py surely

---
+++ neurositycrown/motors_running.py
@@ -0,0 +1,103 @@
+from neurosity import NeurositySDK
+# from dotenv import load_dotenv
+import os
+
+from micromelon import *
+import time
+
+
+NEUROSITY_EMAIL = 'reubenr202@gmail.com'
+NEUROSITY_PASSWORD = 'neuro123'
+NEUROSITY_DEVICE_ID = 'e88770a76231aac1ca98f41e7c9094cd'
+
+# load_dotenv()
+
+neurosity = NeurositySDK({
+    # "device_id": os.getenv("NEUROSITY_DEVICE_ID"),
+    "device_id": NEUROSITY_DEVICE_ID,
+})
+
+# neurosity = NeurositySDK({"device_id": 'e88770a76231aac1ca98f41e7c9094cd'})
+
+neurosity.login({
+    "email": NEUROSITY_EMAIL,
+    "password": NEUROSITY_PASSWORD
+})
+
+rover = False
+forwardDistance = 2
+
+last_trigger = time.time()
+
+
+
+
+def main():
+
+    roverInit()
+
+    # unsubscribe = neurosity.brainwaves_raw(callback)
+    unsubscribe = neurosity.kinesis("rightArm", callback)
+    unsubscribe2 = neurosity.kinesis("leftArm", callback2)
+    
+    # unsubscribe = neurosity.focus(callback)
+
+
+def callback(data):
+    # Switch light off/on
+    print(data['metric'], data['label'], ":", data['confidence'])
+
+    # global Motors
+
+    global last_trigger
+    
+    # time.sleep(1)
+    # Motors.moveDistance(int(forwardDistance))
+    current_trigger = time.time()
+
+    diff = current_trigger - last_trigger
+    print("diff:", diff)
+
+    
+  
+
+    if ((diff > 3) & (data['confidence'] > 0.9)):
+        if Ultrasonic.read() < 400 & Ultrasonic.read()>10:
+            Motors.write(30)
+        if Ultrasonic.read()<10:
+            Motors.write(0)
+        last_trigger = current_trigger
+
+
+def callback2(data):
+    # Switch light off/on
+    print(data['metric'], data['label'], ":", data['confidence'])
+
+    # global Motors
+
+    global last_trigger
+    
+    # time.sleep(1)
+    # Motors.moveDistance(int(forwardDistance))
+    current_trigger = time.time()
+
+    diff = current_trigger - last_trigger
+    print("diff:", diff)
+
+    if ((diff > 3) & (data['confidence'] > 0.9)):
+        Motors.write(0)
+        last_trigger = current_trigger
+    
+    
+def roverInit():
+    rc = RoverController()
+
+    rc.connectBLE(83)
+    rc.startRover()
+    
+
+    rover = True
+
+
+main()
+
